f9090312a364d674e6db3b171a45f2e5ef552fdc
Billionaire-cmd <rabbigyimah7@gmail.com>
2024-11-22 17:35:14 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Rabiotic correct score app 3')
 
-st.write('Hello world!')
+st.write('Betting odds and probabilities calculation of a football (soccer) match!')
